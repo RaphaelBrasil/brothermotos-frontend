f5f4f59fe46a7eb64a1759ce3be3c932d664abac
Raphael <rt.raphael.oliveira@gmail.com>
2024-02-27 19:13:09 -0300

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import Logo from "../../assets/logo.png";
+import * as S from "./styles";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface FormData {
+	email: string;
+	password: string;
+	rememberMe: boolean; // Adicione o campo rememberMe à interface
 }
 
-export default App;
+const Signin = () => {
+	const { signin } = useAuth();
+	const navigate = useNavigate();
+	const {
+		register,
+		handleSubmit,
+		setError,
+		formState: { errors }
+	} = useForm<FormData>();
+
+	const onSubmit: SubmitHandler<FormData> = async (data) => {
+		try {
+			await signin(data.email, data.password);
+			navigate("/home");
+		} catch (err) {
+			setError("password", {
+				message: "E-mail ou senha incorretos"
+			});
+		}
+	};
+
+	const handleSignup = () => {
+		navigate("/signup");
+	};
+
+	return (
+		<S.Container>
+			<S.FormContent onSubmit={handleSubmit(onSubmit)}>
+				<S.Img src={Logo} alt="Logo" />
+				<Input
+					type="text"
+					placeholder="Email ou Login"
+					{...register("email")}
+				/>
+
+				<Input
+					type="password"
+					placeholder="Senha"
+					{...register("password")}
+				/>
+				<S.CheckContainer>
+					<S.CustomCheckbox>
+						<input type="checkbox" {...register("rememberMe")} />
+						<span className="checkmark"></span>
+					</S.CustomCheckbox>
+					<S.Label>Lembrar minha senha</S.Label>
+				</S.CheckContainer>
+
+				<S.LabelError>{errors.password?.message}</S.LabelError>
+
+				<S.ButtonContainer>
+					<Button
+						text="Criar Conta"
+						type="button"
+						onClick={handleSignup}
+						theme="secondary"
+					/>
+					<Button text="Acessar" type="submit" />
+				</S.ButtonContainer>
+			</S.FormContent>
+		</S.Container>
+	);
+};
+
+export default Signin;
